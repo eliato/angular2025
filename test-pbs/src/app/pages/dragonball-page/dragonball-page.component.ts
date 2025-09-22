db92a0f576399cef55e66eeff113a27d288c7ca5
Elias Torres <eliato_@hotmail.com>
2025-09-22 11:32:13 -0600

    logical that list

--- test-pbs/src/app/pages/dragonball-page/dragonball-page.component.ts
+++ test-pbs/src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -15,16 +15,32 @@ interface Character {
 })
 export class DragonballPageComponent {
 
+  name = signal('Goku');
+  power = signal(15000);
+
   characters = signal<Character[]>([
-    {
-      name: 'Goku', power: 9001, id: 1,
-    },
-    {
-      name: 'Vegeta', power: 8001, id: 2,
-    },{
-      name: 'Picoro', power: 5001, id: 3,
-    },
+    {name: 'Goku', power: 9001, id: 1},
+    {name: 'Vegeta', power: 8001, id: 2},
+    {name: 'Picoro', power: 5001, id: 3},
+    {name: 'Yamcha', power: 50, id: 4},
 ]);
 
+addCharacter() {
+
+  if(!this.name() || !this.power() ||this.power() <= 0) return;
+
+  const newCharacter: Character = {
+    name: this.name(),
+    power: this.power(),
+    id: this.characters().length + 1
+  }
+  this.characters.update(characters => [...characters, newCharacter]);
+  this.resetFields();
+ }
+
+ resetFields() {
+  this.name.set('');
+  this.power.set(0);
+ }
 
 }
