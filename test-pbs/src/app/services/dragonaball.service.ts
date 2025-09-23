097a42247c2eab5ecef36520b7dc8354afc667b4
Elias Torres <eliato_@hotmail.com>
2025-09-23 14:10:26 -0600

    adding service

--- test-pbs/src/app/services/dragonaball.service.ts
+++ test-pbs/src/app/services/dragonaball.service.ts
@@ -1,5 +1,10 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+const loadFromLocalStorage = (): Character[] => {
+  const data = localStorage.getItem('characters');
+  return data ? JSON.parse(data) : [];
+}
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +17,10 @@ export class DragonaballService {
     /* {name: 'Picoro', power: 5001, id: 3},
     {name: 'Yamcha', power: 50, id: 4}, */
 ]);
+
+saveToLocalStorage = effect(() => {
+  localStorage.setItem('characters', JSON.stringify(this.characters()));
+});
 
 addCharacter(character: Character) {
 this.characters.update(characters => [...characters, character]);
