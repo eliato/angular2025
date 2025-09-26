f9c1ecf139276a6f87eadc59e8e5a8673fb33384
Elias Torres <eliato_@hotmail.com>
2025-09-26 15:35:47 -0600

    adding funtionality search

--- gits-app/src/app/gifs/components/search-page/search-page.component.ts
+++ gits-app/src/app/gifs/components/search-page/search-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { GifsListComponent } from "../gifs-list/gifs-list.component";
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifsListComponent],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.css',
 
 })
-export default class SearchPageComponent { }
+export default class SearchPageComponent {
+onSearch(query: string) {
+console.log(query);
+}
+}
