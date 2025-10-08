f33f3b416cfb259a35a5d6943b725c3104d05621
Elias Torres <eliato_hotmail.com>
2025-10-08 13:20:04 -0600

    add profile terminal

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+oh-my-posh init pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/bubbles.omp.json | Invoke-Expression
+Import-Module -Name Terminal-Icons
+Import-Module -Name PSReadLine
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
